824845a7f476ba5b92505d59ddb8d408f259c433
Dido <adhaithaam@gmail.com>
2024-08-08 11:06:01 +0300

    RadixUI + Next.js + Shadcn UI goodnight

--- clientside/src/components/blocks/Header.tsx
+++ clientside/src/components/blocks/Header.tsx
@@ -1,17 +1,10 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-NavigationMenu,
-NavigationMenuContent,
-NavigationMenuItem,
-NavigationMenuLink,
-NavigationMenuList,
-NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
 import { Menu, MoveRight, X } from "lucide-react";
 import { useState } from "react";
 import Link from "next/link";
+import { FaGithub } from "react-icons/fa6";
 
 export const Header1 = () => {
 const navigationItems = [
@@ -71,65 +64,17 @@ return (
     <header className="w-full z-40 fixed top-0 left-0 bg-background">
     <div className="container relative mx-auto min-h-20 flex gap-4 flex-row lg:grid lg:grid-cols-3 items-center">
         <div className="justify-start items-center gap-4 lg:flex hidden flex-row">
-        <NavigationMenu className="flex justify-start items-start">
-            <NavigationMenuList className="flex justify-start gap-4 flex-row">
-            {navigationItems.map((item) => (
-                <NavigationMenuItem key={item.title}>
-                {item.href ? (
-                    <>
-                    <NavigationMenuLink>
-                        <Button variant="ghost">{item.title}</Button>
-                    </NavigationMenuLink>
-                    </>
-                ) : (
-                    <>
-                    <NavigationMenuTrigger className="font-medium text-sm">
-                        {item.title}
-                    </NavigationMenuTrigger>
-                    <NavigationMenuContent className="!w-[450px] p-4">
-                        <div className="flex flex-col lg:grid grid-cols-2 gap-4">
-                        <div className="flex flex-col h-full justify-between">
-                            <div className="flex flex-col">
-                            <p className="text-base">{item.title}</p>
-                            <p className="text-muted-foreground text-sm">
-                                {item.description}
-                            </p>
-                            </div>
-                            <Button size="sm" className="mt-10">
-                            Book a call today
-                            </Button>
-                        </div>
-                        <div className="flex flex-col text-sm h-full justify-end">
-                            {item.items?.map((subItem) => (
-                            <NavigationMenuLink
-                                href={subItem.href}
-                                key={subItem.title}
-                                className="flex flex-row justify-between items-center hover:bg-muted py-2 px-4 rounded"
-                            >
-                                <span>{subItem.title}</span>
-                                <MoveRight className="w-4 h-4 text-muted-foreground" />
-                            </NavigationMenuLink>
-                            ))}
-                        </div>
-                        </div>
-                    </NavigationMenuContent>
-                    </>
-                )}
-                </NavigationMenuItem>
-            ))}
-            </NavigationMenuList>
-        </NavigationMenu>
+      <Button variant="ghost" className="hidden md:inline">
+            Home
+        </Button>
         </div>
         <div className="flex lg:justify-center">
         <p className="font-semibold">Scraddit</p>
         </div>
         <div className="flex justify-end w-full gap-4">
-        <Button variant="ghost" className="hidden md:inline">
-            Book a demo
-        </Button>
         <div className="border-r hidden md:inline"></div>
-        <Button variant="outline">Sign in</Button>
-        <Button>Get started</Button>
+        <Button variant="outline">FAQ</Button>
+            <Button><FaGithub></FaGithub> </Button>
         </div>
         <div className="flex w-12 shrink lg:hidden items-end justify-end">
         <Button variant="ghost" onClick={() => setOpen(!isOpen)}>
